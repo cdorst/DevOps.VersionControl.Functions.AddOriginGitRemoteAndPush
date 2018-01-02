90b1b0f685fbe5a78d36a65e8561aab8dc0b620c
Christopher Dorst <chris@dorst.email>
2018-01-02 02:32:59 -0600

    Add RemoteAdderAndChangePusher

---
+++ DevOps.VersionControl.Functions.AddOriginGitRemoteAndPush/RemoteAdderAndChangePusher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static DevOps.VersionControl.Functions.RunGitPushCommand.GitPushCommandRunner;
+using static DevOps.VersionControl.Functions.RunGitRemoteCommand.GitRemoteCommandRunner;
+
+namespace DevOps.VersionControl.Functions.AddOriginGitRemoteAndPush
+{
+    public static class RemoteAdderAndChangePusher
+    {
+        private const string add = nameof(add);
+        private const string master = nameof(master);
+        private const string origin = nameof(origin);
+
+        public static void AddRemoteAndPush(string repositoryDirectory, Uri remoteUri, string branchName = master, string remoteName = origin)
+        {
+            Remote(repositoryDirectory,
+                add,
+                remoteName,
+                remoteUri.ToString());
+            Push(repositoryDirectory,
+                remoteName,
+                branchName);
+        }
+    }
+}
